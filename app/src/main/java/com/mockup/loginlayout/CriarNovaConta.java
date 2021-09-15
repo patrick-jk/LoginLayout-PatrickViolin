0772b8a204d1d925e5352e3a358ab4fc44d94a5b
xJoestar <patrickviolin08@gmail.com>
2021-09-15 17:34:25 -0300

    Login Layouts - Patrick Peixoto Violin

---
+++ app/src/main/java/com/mockup/loginlayout/CriarNovaConta.java
@@ -0,0 +1,14 @@
+package com.mockup.loginlayout;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CriarNovaConta extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.criar_nova_conta);
+    }
+}
---
+++ app/src/main/java/com/mockup/loginlayout/TelaInicialCadastro.java
@@ -0,0 +1,14 @@
+package com.mockup.loginlayout;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TelaInicialCadastro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tela_inicial_cadastro);
+    }
+}
